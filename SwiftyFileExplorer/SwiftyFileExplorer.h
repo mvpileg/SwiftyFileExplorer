e0097f3d8a09380949f191bc0f82cfc6c3592373
Matthew Pileggi <matt@matts-mbp.home>
2017-01-03 12:18:13 -0500

    Initial Commit

---
+++ SwiftyFileExplorer/SwiftyFileExplorer.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyFileExplorer.h
+//  SwiftyFileExplorer
+//
+//  Created by Matthew Pileggi on 1/3/17.
+//  Copyright © 2017 Matthew Pileggi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyFileExplorer.
+FOUNDATION_EXPORT double SwiftyFileExplorerVersionNumber;
+
+//! Project version string for SwiftyFileExplorer.
+FOUNDATION_EXPORT const unsigned char SwiftyFileExplorerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyFileExplorer/PublicHeader.h>
+
+
